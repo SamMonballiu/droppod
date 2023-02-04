259af64cf65ad510d2306a21e568e4ec2f73f9ae
SamMonballiu <samsonmonballiu@hotmail.com>
2023-02-04 10:21:24 +0000

    add multer

--- server.ts
+++ server.ts
@@ -1,14 +1,30 @@
 import express, { Request, Response } from "express";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, __dirname + "/public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 const app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/upload_files", (req: Request, res: Response) => {
-    console.log(req.body);
-});
+app.post(
+  "/upload_files",
+  upload.array("files"),
+  (_: Request, res: Response) => {
+    res.status(200).send("ok");
+  }
+);
 
 app.listen(4004, () => {
-    console.log("Server started...");
+  console.log("Server started...");
 });
