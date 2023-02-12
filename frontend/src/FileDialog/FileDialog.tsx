fc55ba743f5621aca5737577670d5d36d23a7fff
Sam Monballiu <samsonmonballiu@hotmail.com>
2023-02-12 15:42:02 +0100

    use ImagePreview on FileDialog

--- frontend/src/FileDialog/FileDialog.tsx
+++ frontend/src/FileDialog/FileDialog.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
-import { FileInfo } from "../../../models/fileinfo";
+import { FileInfo, getSize } from "../../../models/fileinfo";
 import Dialog from "../Dialog/Dialog";
+import FileSize from "../FileSize/FileSize";
 import ImagePreview from "../ImagePreview.tsx/ImagePreview";
 import styles from "./FileDialog.module.scss";
 
@@ -11,10 +12,23 @@ interface Props {
 }
 
 const FileDialog: FC<Props> = ({ isOpen, onClose, file }) => {
+  const dimensionsInfo = `${file.dimensions?.width}x${file.dimensions?.height}px`;
   return (
-    <Dialog isOpen={isOpen} onClose={onClose} title={file.filename}>
-      <ImagePreview file={file} />
-      <p>Test</p>
+    <Dialog isOpen={isOpen} onClose={onClose}>
+      <div>
+        <ImagePreview file={file} className={styles.image} />
+        <div className={styles.info}>
+          <h3>{file.filename}</h3>
+          <p>{new Date(file.dateAdded).toLocaleDateString()}</p>
+          <p>
+            <FileSize file={file} />
+          </p>
+          {file.dimensions && <p>{dimensionsInfo}</p>}
+          <a href={file.fullPath} target="_blank">
+            link
+          </a>
+        </div>
+      </div>
     </Dialog>
   );
 };
