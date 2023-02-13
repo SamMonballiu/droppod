d6f71e30136f31e38b80e7a4c83bd3340cfc094a
Sam Monballiu <samsonmonballiu@hotmail.com>
2023-02-13 11:58:22 +0100

    add FileProperties component

--- frontend/src/FileDialog/FileDialog.tsx
+++ frontend/src/FileDialog/FileDialog.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { FileInfo, getSize } from "../../../models/fileinfo";
 import Dialog from "../Dialog/Dialog";
-import FileSize from "../FileSize/FileSize";
+import FileProperties from "../FileProperties/FileProperties";
 import ImagePreview from "../ImagePreview.tsx/ImagePreview";
 import styles from "./FileDialog.module.scss";
 
@@ -12,22 +12,11 @@ interface Props {
 }
 
 const FileDialog: FC<Props> = ({ isOpen, onClose, file }) => {
-  const dimensionsInfo = `${file.dimensions?.width}x${file.dimensions?.height}px`;
   return (
     <Dialog isOpen={isOpen} onClose={onClose}>
       <div>
         <ImagePreview file={file} className={styles.image} />
-        <div className={styles.info}>
-          <h3>{file.filename}</h3>
-          <p>{file.dateAdded.toLocaleDateString()}</p>
-          <p>
-            <FileSize file={file} />
-          </p>
-          {file.dimensions && <p>{dimensionsInfo}</p>}
-          <a href={file.fullPath} target="_blank">
-            link
-          </a>
-        </div>
+        <FileProperties file={file} />
       </div>
     </Dialog>
   );
