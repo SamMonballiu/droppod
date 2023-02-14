9fd9e1759fc6c21ea28424954802227bc57e521f
Sam Monballiu <samsonmonballiu@hotmail.com>
2023-02-14 17:14:17 +0100

    memoize Gallery

--- frontend/src/Files/Files.tsx
+++ frontend/src/Files/Files.tsx
@@ -10,7 +10,7 @@ import { useSortedList } from "../hooks/useSortedList";
 import FileSortOptions, { SortOption } from "./FileSortOptions";
 import { FilesResponse } from "../../../models/response";
 import FileDialog from "../FileDialog/FileDialog";
-import Gallery from "../Gallery/Gallery";
+import { MemoizedGallery } from "../Gallery/Gallery";
 import { useThumbnails } from "../hooks/useThumbnails";
 
 interface Props {
@@ -53,76 +53,78 @@ const Files: FC<Props> = ({ data }) => {
           file={selectedFile}
         />
       )}
-      <div className={styles.settings}>
-        {view === "grid" ? (
-          <>
-            <FileSortOptions
-              options={sortOptions}
-              value={sortProperty!}
-              onChange={(opt) => {
-                //@ts-ignore
-                handleSort(opt);
-              }}
-              isDescending={isDescending}
-            />
-            <div className={styles.zoomSlider}>
-              <TbTelescope />
-              <input
-                type="range"
-                min="1"
-                max="4"
-                value={zoom}
-                onChange={(e) =>
-                  setZoom(parseInt(e.target.value) as FileGridZoom)
-                }
-              />
-            </div>
-          </>
-        ) : (
-          <div>&nbsp;</div>
-        )}
-
-        <div className={styles.icons}>
-          <MdOutlineListAlt
-            className={cx({ [styles.active]: view === "list" })}
-            onClick={() => setView("list")}
-          />
-          <MdGridView
-            className={cx({ [styles.active]: view === "grid" })}
-            onClick={() => setView("grid")}
-          />
-          <MdOutlinePhoto
-            // @ts-ignore
-            className={cx({ [styles.active]: view === "gallery" })}
-            onClick={() => setView("gallery")}
-          />
-        </div>
-      </div>
-
       {view === "gallery" && (
-        <Gallery
+        <MemoizedGallery
           files={getSorted().filter(isImage)}
           onClose={() => setView("grid")}
         />
       )}
 
-      {view === "list" ? (
-        <FileList
-          files={getSorted()}
-          onSort={sort}
-          onSelectFile={handleSelectFile}
-        />
-      ) : (
-        <MemoizedGrid
-          files={getSorted()}
-          zoom={zoom}
-          onSelectFile={handleSelectFile}
-          thumbnails={thumbnails}
-        />
-      )}
+      <div className={cx({ [styles.hidden]: view === "gallery" })}>
+        <div className={styles.settings}>
+          {view === "grid" ? (
+            <>
+              <FileSortOptions
+                options={sortOptions}
+                value={sortProperty!}
+                onChange={(opt) => {
+                  //@ts-ignore
+                  handleSort(opt);
+                }}
+                isDescending={isDescending}
+              />
+              <div className={styles.zoomSlider}>
+                <TbTelescope />
+                <input
+                  type="range"
+                  min="1"
+                  max="4"
+                  value={zoom}
+                  onChange={(e) =>
+                    setZoom(parseInt(e.target.value) as FileGridZoom)
+                  }
+                />
+              </div>
+            </>
+          ) : (
+            <div>&nbsp;</div>
+          )}
 
-      <div className={styles.info}>
-        Free space: {(data.freeSpace / 1024 / 1024).toFixed(2)} mb
+          <div className={styles.icons}>
+            <MdOutlineListAlt
+              className={cx({ [styles.active]: view === "list" })}
+              onClick={() => setView("list")}
+            />
+            <MdGridView
+              className={cx({ [styles.active]: view === "grid" })}
+              onClick={() => setView("grid")}
+            />
+            <MdOutlinePhoto
+              // @ts-ignore
+              className={cx({ [styles.active]: view === "gallery" })}
+              onClick={() => setView("gallery")}
+            />
+          </div>
+        </div>
+
+        {view === "list" ? (
+          <FileList
+            files={getSorted()}
+            onSort={sort}
+            onSelectFile={handleSelectFile}
+          />
+        ) : (
+          <MemoizedGrid
+            files={getSorted()}
+            zoom={zoom}
+            onSelectFile={handleSelectFile}
+            thumbnails={thumbnails}
+          />
+        )}
+
+        <div className={styles.info}>
+          Free space: {(data.freeSpace / 1024 / 1024).toFixed(2)} mb
+        </div>
       </div>
     </>
   );
