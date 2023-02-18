8186cd160f4c1997efde49f244fdbda577912b2d
Sam Monballiu <samsonmonballiu@hotmail.com>
2023-02-18 13:47:19 +0100

    add Breadcrumbs component

---
+++ frontend/src/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,41 @@
+import { GoChevronRight } from "react-icons/go";
+import { AiOutlineHome } from "react-icons/ai";
+import { FC } from "react";
+import styles from "./Breadcrumbs.module.scss";
+import cx from "classnames";
+
+interface Props {
+  path: string;
+  onClick: (destination: string) => void;
+}
+
+const Breadcrumbs: FC<Props> = ({ path, onClick }) => {
+  const pathElements = path.split("/");
+
+  const mapped = pathElements.map((el, idx) => {
+    const targetUrl = pathElements.slice(0, idx + 1).join("/");
+    const isLastElement = idx === pathElements.length - 1;
+    return (
+      <div className={styles.crumb} key={targetUrl}>
+        {targetUrl !== "" ? (
+          <span
+            className={cx({ [styles.clickable]: !isLastElement })}
+            onClick={isLastElement ? undefined : () => onClick(targetUrl)}
+          >
+            {el}
+          </span>
+        ) : (
+          <AiOutlineHome
+            className={styles.clickable}
+            onClick={() => onClick(targetUrl)}
+          />
+        )}
+
+        {!isLastElement && <GoChevronRight className={styles.chevron} />}
+      </div>
+    );
+  });
+  return <section className={styles.container}>{mapped}</section>;
+};
+
+export default Breadcrumbs;
