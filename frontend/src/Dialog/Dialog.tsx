f76c888491efc3ff9c3ba6f1323402f7040faded
Sam Monballiu <samsonmonballiu@hotmail.com>
2023-03-24 13:28:06 +0100

    add optional buttons prop to Dialog

--- frontend/src/Dialog/Dialog.tsx
+++ frontend/src/Dialog/Dialog.tsx
@@ -2,13 +2,25 @@ import { Dialog as HeadlessDialog } from "@headlessui/react";
 import React, { FC, useRef } from "react";
 import styles from "./Dialog.module.scss";
 
-interface DialogProps extends React.PropsWithChildren {
+export interface DialogProps extends React.PropsWithChildren {
   title?: string;
   isOpen: boolean;
   onClose: () => void;
+  buttons?: {
+    label: string;
+    className?: string;
+    onClick: () => void;
+    disabled?: boolean;
+  }[];
 }
 
-const Dialog: FC<DialogProps> = ({ title, isOpen, onClose, children }) => {
+const Dialog: FC<DialogProps> = ({
+  title,
+  isOpen,
+  onClose,
+  children,
+  buttons,
+}) => {
   const dialogRef = useRef<HTMLElement | undefined>();
   const backdropRef = useRef<HTMLElement | undefined>();
 
@@ -32,6 +44,19 @@ const Dialog: FC<DialogProps> = ({ title, isOpen, onClose, children }) => {
         <HeadlessDialog.Panel ref={dialogRef} className={styles.panel}>
           {title && <HeadlessDialog.Title>{title}</HeadlessDialog.Title>}
           {children}
+          {buttons && (
+            <div className={styles.buttons}>
+              {buttons.map((btn) => (
+                <button
+                  disabled={btn.disabled ?? false}
+                  className={btn.className}
+                  onClick={btn.onClick}
+                >
+                  {btn.label}
+                </button>
+              ))}
+            </div>
+          )}
         </HeadlessDialog.Panel>
       </div>
     </HeadlessDialog>
