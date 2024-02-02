ea7e8856e5fc09274ceeae759fb4d65b7a95eb70
Sam Monballiu <samsonmonballiu@hotmail.com>
2024-02-02 11:20:35 +0100

    clean up the crawler

--- folder-crawler-go/crawler.go
+++ folder-crawler-go/crawler.go
@@ -5,18 +5,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 
-	"image"
-	_ "image/gif"
-	_ "image/jpeg"
-	_ "image/png"
-	"path"
-
 	"encoding/json"
-
-	"github.com/disintegration/imaging"
 )
 
 type FilesResponse struct {
@@ -45,7 +36,6 @@ type FileInfo struct {
 	DateAdded    time.Time `json:"dateAdded"`
 	IsFolder     bool      `json:"isFolder"`
 	Rating       byte      `json:"rating"`
-	// Dimensions   *Dimensions `json:"dimensions"`
 }
 
 type FolderInfo struct {
@@ -58,7 +48,6 @@ type FolderInfo struct {
 
 func main() {
 	http.HandleFunc("/list", listHandler)
-	//http.HandleFunc("/thumbnail", thumbnailHandler)
 	fmt.Println("Listening on port 5566...")
 	http.ListenAndServe(":5566", nil)
 }
@@ -67,7 +56,6 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Query().Get("path")
 	basePath := r.URL.Query().Get("basePath")
 	fullPath := filepath.Join(basePath, path)
-	//fmt.Printf("BasePath: %v ][ Path: %v ][ FullPath: %v\n", basePath, path, fullPath)
 	if path == "" || basePath == "" {
 		http.Error(w, "Missing path/basePath parameter", http.StatusBadRequest)
 		return
@@ -80,7 +68,6 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 			IsSuccess: false,
 			Error:     err.Error(),
 		}
-		//http.Error(w, err.Error(), http.StatusInternalServerError)
 		jsonBytes, _ := json.MarshalIndent(response, "", "")
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(jsonBytes)
@@ -105,7 +92,6 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 		} else {
 			info, _ := file.Info()
 			filename := file.Name()
-			// isImage, height, width, orientation := checkImageProperties(filepath.Join(fullPath, filename))
 
 			fileInfo := FileInfo{
 				Filename:     filename,
@@ -116,9 +102,6 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 				DateAdded:    info.ModTime(),
 				Rating:       0,
 			}
-			// if isImage {
-			// 	fileInfo.Dimensions = &Dimensions{Height: height, Width: width, Orientation: orientation}
-			// }
 
 			subFiles = append(subFiles, fileInfo)
 		}
@@ -136,105 +119,4 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	jsonBytes, _ := json.MarshalIndent(response, "", "")
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(jsonBytes)
-}
-
-// This function takes a file path as a string and returns a boolean indicating whether it is an image file, and if so, its height, width and orientation as integers.
-func checkImageProperties(filePath string) (bool, int, int, byte) {
-	// Get the file extension from the file path
-	ext := strings.ToLower(path.Ext(filePath))
-
-	// Check if the extension is one of the supported image formats
-	if ext == ".gif" || ext == ".jpg" || ext == ".jpeg" || ext == ".png" {
-		// Open the file and decode it as an image
-		file, err := os.Open(filePath)
-		if err != nil {
-			// Handle the error
-			return false, 0, 0, 0
-		}
-		defer file.Close()
-
-		config, _, err := image.DecodeConfig(file)
-		if err != nil {
-			// Handle the error
-			return false, 0, 0, 0
-		}
-
-		height := config.Height
-		width := config.Width
-
-		// img, _, err := image.Decode(file)
-		// if err != nil {
-		// 	// Handle the error
-		// 	return false, 0, 0, 0
-		// }
-
-		// // Get the image bounds and dimensions
-		// bounds := img.Bounds()
-		// height := bounds.Max.Y - bounds.Min.Y
-		// width := bounds.Max.X - bounds.Min.X
-
-		// Determine the orientation based on the aspect ratio
-		// 0: square, 1: portrait, 2: landscape
-		var orientation byte
-		orientation = 0
-		if height > width {
-			orientation = 1
-		} else if width > height {
-			orientation = 2
-		}
-
-		// Return true and the image properties
-		return true, height, width, orientation
-	} else {
-		// Return false and zero values
-		return false, 0, 0, 0
-	}
-}
-
-func thumbnailHandler(w http.ResponseWriter, r *http.Request) {
-	path := r.URL.Query().Get("path")
-	basePath := r.URL.Query().Get("basePath")
-	if path == "" || basePath == "" {
-		http.Error(w, "Missing path/basePath parameter", http.StatusBadRequest)
-		return
-	}
-
-	fmt.Printf("THUMBNAIL: %v + %v\n", basePath, path)
-	fmt.Println(filepath.Join(basePath, path))
-
-	img, err := imaging.Open(filepath.Join(basePath, path))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	thumbnail := imaging.Thumbnail(img, 100, 100, imaging.Lanczos) // 0 was 300
-	ext := strings.ToLower(filepath.Ext(path))
-	fmt.Printf("ext: %v", ext)
-
-	// Set the appropriate content type based on the file extension
-	switch ext {
-	case ".jpg", ".jpeg":
-		w.Header().Set("Content-Type", "image/jpeg")
-	case ".png":
-		w.Header().Set("Content-Type", "image/png")
-	default:
-		http.Error(w, "Unsupported file type", http.StatusBadRequest)
-		return
-	}
-
-	err = imaging.Encode(w, thumbnail, imaging.JPEG)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	//thumbnailPath := strings.TrimSuffix(path, ext) + "_thumbnail" + ext
-	// err = imaging.Save(thumbnail, thumbnailPath)
-	// if err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// 	return
-	// }
-	// http.ServeFile(w, r, thumbnailPath)
-
 }
